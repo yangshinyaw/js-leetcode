2870ba2fccae02c92c7d9798572ea81c4f92acbb
Shin <syty03@gmail.com>
2025-05-19 22:47:56 +0800

    odd or even problem

---
+++ day1/even-or-odd.js
@@ -0,0 +1,11 @@
+// Question: Write a function that checks if a number is even or odd.
+
+// Date Solved: 2025-05-19
+
+const number = prompt("Enter a number: ");
+
+if (number % 2 ==  0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
