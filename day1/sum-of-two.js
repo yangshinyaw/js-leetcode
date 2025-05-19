cf5cf9d7b9dc4e28f069fd2908b9c4464b10d17c
Shin <syty03@gmail.com>
2025-05-19 22:42:54 +0800

    initial commit

---
+++ day1/sum-of-two.js
@@ -0,0 +1,10 @@
+// Question: Write a function to return the sum of two numbers.
+
+// Date Solved: 2025-05-19
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(2, 3)); 
+console.log(sum(3, 4));
