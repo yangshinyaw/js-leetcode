0bd8cf2bd297cd4e4cb23053c0fec1fe34490324
Shin <syty03@gmail.com>
2025-05-20 23:33:16 +0800

    print all numbers from 1 to n

---
+++ day2/print-all-numbers.js
@@ -0,0 +1,11 @@
+// Write a function to print all numbers from 1 to n.
+
+// Date Solved: 2025-05-20
+
+function printAllNumbers(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log(i);
+    }
+}
+
+console.log(printAllNumbers(9));
