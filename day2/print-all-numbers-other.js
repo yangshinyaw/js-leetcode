a0ec337a3b79e8e0adb1f7553a1c1f7709c18a68
Shin <syty03@gmail.com>
2025-05-21 23:44:52 +0800

    other solution for print all numbers from 1 to n

--- day2/print-all-numbers-other.js
+++ day2/print-all-numbers-other.js
@@ -15,3 +15,24 @@ function printNumbersDoWhile(n) {
         i++;
     } while (i <= n);
 }
+
+// Using Array methods
+function printNumbersArray(n) {
+    Array.from({ length: n }, (_, index) => {
+        console.log(index + 1);
+    });
+}
+
+// Using Array.forEach with spread operator
+function printNumbersSpread(n) {
+    [...Array(n)].forEach((_, index) => {
+        console.log(index + 1);
+    });
+}
+
+// Using recursion
+function printNumbersRecursive(n, current = 1) {
+    if (current > n) return;
+    console.log(current);
+    printNumbersRecursive(n, current + 1);
+}
