7cb8a2d9e3ea6327cadf2942ef88510106888a20
Shin <syty03@gmail.com>
2025-05-21 23:44:40 +0800

    other solution for print all numbers from 1 to n

---
+++ day2/print-all-numbers-other.js
@@ -0,0 +1,17 @@
+// Using while loop
+function printNumbersWhile(n) {
+    let i = 1;
+    while (i <= n) {
+        console.log(i);
+        i++;
+    }
+}
+
+// Using do-while loop
+function printNumbersDoWhile(n) {
+    let i = 1;
+    do {
+        console.log(i);
+        i++;
+    } while (i <= n);
+}
