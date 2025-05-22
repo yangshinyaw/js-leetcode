884d3de91e44f773eb8dd306630b3457a19003c3
Shin <syty03@gmail.com>
2025-05-22 22:37:17 +0800

    return factorial other solutions

--- day2/return-factorial-other.js
+++ day2/return-factorial-other.js
@@ -9,4 +9,20 @@ function factorialWhile(n) {
     return result;
 }
 
+// Recursive approach
+function factorialRecursive(n) {
+    if (n <= 1) return 1;
+    return n * factorialRecursive(n - 1);
+}
+
+// Using Array.from() and reduce()
+function factorialReduce(n) {
+    return Array.from({ length: n }, (_, i) => i + 1)
+                .reduce((acc, num) => acc * num, 1);
+}
+
 console.log(factorialWhile(5));
+console.log(factorialRecursive(5));
+console.log(factorialReduce(5));
+
+
