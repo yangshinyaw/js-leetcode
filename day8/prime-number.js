7fc904ea0230f5964ad6f0febaaed1956ec458cc
Shin Yaw Yang <syty03@gmail.com>
2025-06-08 22:19:59 +0800

    other solution for prime numbers

--- day8/prime-number.js
+++ day8/prime-number.js
@@ -20,3 +20,27 @@ function isPrime2(n) {
     }
     return true;
 }
+
+// Solution 3: Functional approach using array methods
+function isPrime3(n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    
+    const limit = Math.sqrt(n);
+    return !Array.from({length: Math.floor(limit) - 1}, (_, i) => i + 2)
+        .some(divisor => n % divisor === 0);
+}
+
+// Test all three solutions
+const testNumbers = [2, 3, 4, 17, 25, 29, 97, 100, 101];
+
+console.log("Testing prime checking functions:");
+console.log("Number\tSol1\tSol2\tSol3");
+console.log("-".repeat(30));
+
+testNumbers.forEach(num => {
+    const result1 = isPrime1(num);
+    const result2 = isPrime2(num);
+    const result3 = isPrime3(num);
+    console.log(`${num}\t${result1}\t${result2}\t${result3}`);
+});
