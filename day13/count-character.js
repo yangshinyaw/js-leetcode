079b0e97dc512cfc97aafe69b53abb239c5cfb4a
Shin Yaw Yang <syty03@gmail.com>
2025-06-19 23:04:42 +0800

    count how many times a character appear other solution

--- day13/count-character.js
+++ day13/count-character.js
@@ -13,3 +13,25 @@ function countCharLoop(str, char) {
 function countCharSplit(str, char) {
     return str.split(char).length - 1;
 }
+
+// Solution 3: Using filter() with spread operator (functional approach)
+function countCharFilter(str, char) {
+    return [...str].filter(c => c === char).length;
+}
+
+// Test all three solutions
+const testString = "hello world";
+const targetChar = "l";
+
+console.log("Testing string:", testString);
+console.log("Counting character:", targetChar);
+console.log("\nResults:");
+console.log("Loop method:", countCharLoop(testString, targetChar));
+console.log("Split method:", countCharSplit(testString, targetChar));
+console.log("Filter method:", countCharFilter(testString, targetChar));
+
+// Additional test cases
+console.log("\n--- Additional Tests ---");
+console.log('countCharLoop("javascript", "a"):', countCharLoop("javascript", "a"));
+console.log('countCharSplit("programming", "m"):', countCharSplit("programming", "m"));
+console.log('countCharFilter("banana", "n"):', countCharFilter("banana", "n"));
